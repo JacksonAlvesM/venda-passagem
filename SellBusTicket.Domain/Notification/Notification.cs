77356b419d6926791cd8e69de0ea49b372cc1170
Jackson Alves <66750478+JacksonAlvesM@users.noreply.github.com>
2025-05-09 15:01:43 -0300

    [ feat ]-  adiciona NotificationContext para tratamento de erros

---
+++ SellBusTicket.Domain/Notification/Notification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SellBusTicket.Domain.Notification
+{
+    public class Notification
+    {
+        public string Key { get; }
+        public string Message { get; }
+
+        public Notification(string message, string key = "")
+        {
+            Key = key;
+            Message = message;
+        }
+    }
+}
---
+++ SellBusTicket.Domain/Notification/NotificationContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SellBusTicket.Domain.Notification
+{
+    public class NotificationContext
+    {
+        private readonly List<Notification> _notifications = new();
+
+        public IReadOnlyCollection<Notification> Notifications => _notifications;
+        public bool HasNotifications => _notifications.Any();
+
+        public void AddNotification(string message, string key = "") =>
+            _notifications.Add(new Notification(message, key));
+
+        public void AddNotifications(IEnumerable<Notification> notifications) =>
+            _notifications.AddRange(notifications);
+
+        public void Clear() => _notifications.Clear();
+        public IEnumerable<string> GetNotifications() =>
+            _notifications.Select(n => n.Message);
+    }
+}
